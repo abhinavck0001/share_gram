51d80023b4a36fceb4d4df99f429982ee1eb788f
abhinav ck <abhinavck0001@gmail.com>
2022-10-27 16:35:54 +0530

    navigation to pages

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import  store from "./store/ReduxStore.js"
+import store from "./store/ReduxStore.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <Provider store = {store}>
-    <App/>
+  <Provider store={store}>
+    <BrowserRouter>
+     <Routes>
+      <Route path="*" element = {<App/>}/>
+     </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
