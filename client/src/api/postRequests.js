73ae390b64efc822b0adee37e3bd4d3828000025
abhinav ck <abhinavck0001@gmail.com>
2022-11-03 13:56:47 +0530

    like the post

--- client/src/api/postRequests.js
+++ client/src/api/postRequests.js
@@ -11,5 +11,5 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 //     return req;
 //   });
 
-export const getTimelinePosts= (id)=> API.get(`/posts/${id}/timeline`);
-// export const likePost=(id, userId)=>API.put(`posts/${id}/like`, {userId: userId})
+export const getTimelinePosts= (id)=> API.get(`/post/${id}/timeline`);
+export const likePost=(id, userId)=>API.put(`post/${id}/like`, {userId: userId})
